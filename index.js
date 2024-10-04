cdab649f714ceb1e1bac47a8779df5efbed02aa2
horrocksm <horrocksm@ltegroup.co.uk>
2024-10-04 14:43:16 +0100

    Added version relying more on bootstrap classes

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 
 document.querySelectorAll('.mn-trigger').forEach(function(trigger) {
-    trigger.addEventListener('click', function() {
-        var target = document.querySelector('.mn-wrapper');
+    trigger.addEventListener('click', function(event) {
+        event.preventDefault();
+        let targetId = trigger.getAttribute('data-mn-target');
+        var target = document.querySelector(targetId);
         target.classList.toggle('mn-open');
     });
 });
