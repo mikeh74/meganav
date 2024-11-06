5f4a7e538b867e370de9cfc168b21d9af017394e
horrocksm <horrocksm@ltegroup.co.uk>
2024-11-06 14:50:28 +0000

    Code changes

--- index.js
+++ index.js
@@ -1,9 +1,41 @@
 
-document.querySelectorAll('.mn-trigger').forEach(function(trigger) {
-    trigger.addEventListener('click', function(event) {
+// query selector all using data attribute data-mn-target
+document.querySelectorAll('[data-mn-target]').forEach(trigger => {
+
+    // loop through all trigger elements
+    trigger.addEventListener('click', function (event) {
+
         event.preventDefault();
+        // get the target id from the data attribute
         let targetId = trigger.getAttribute('data-mn-target');
-        var target = document.querySelector(targetId);
-        target.classList.toggle('mn-open');
+
+        // this is the element that we are selecting
+        let target = document.querySelector(targetId);
+
+        // wrapped in animation frame which might be overkill but intended
+        // to ensure that all repaints happen in the same frame
+        requestAnimationFrame(() => {
+            if (target.classList.contains('mn-open')) {
+                target.classList.remove('mn-open');
+            } else {
+                // close any other open menus before we open the current one
+                closeMenu();
+                target.classList.add('mn-open');
+                document.body.classList.add('active');
+            }
+        });
     });
+});
+
+function closeMenu() {
+    document.querySelectorAll('.mn-wrapper').forEach(target => {
+        target.classList.remove('mn-open');
+    });
+    document.body.classList.remove('active');
+};
+
+document.addEventListener('click', function (event) {
+    if (!event.target.closest('.mn-wrapper') && !event.target.closest('.mn-trigger')) {
+        closeMenu();
+    }
 });
