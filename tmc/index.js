13bd0891925afeb31212392673f3c358664890aa
Mike Horrocks <michael.horrocks@hotmail.co.uk>
2025-08-19 20:57:20 +0100

    Tidying up tabindex

--- tmc/index.js
+++ tmc/index.js
@@ -12,17 +12,44 @@ mainNavImgs.forEach(i => {
     };
 });
 
-// wire up the main nav
 const mainNavSecondaryItems = document.querySelectorAll('.main-nav-secondary-item > a');
+const submenus = document.querySelectorAll('.main-nav-secondary-item .submenu');
+
+const resetSubmenus = () => {
+    submenus.forEach(submenu => {
+        submenu.classList.remove('submenu-active');
+        const links = submenu.querySelectorAll('a');
+        links.forEach(link => {
+            link.tabIndex = -1;
+        });
+    });
+};
 
 mainNavSecondaryItems.forEach(item => {
+
+    const target = item.getAttribute('href');
+    const targetElement = document.querySelector(target);
+
     item.addEventListener('click', (e) => {
         e.preventDefault();
-        const target = item.getAttribute('href');
 
-        if (target.startsWith('#')) {
-            const targetElement = document.querySelector(target);
-            targetElement.classList.toggle('submenu-active');
+        const links = targetElement.querySelectorAll('.submenu a');
+
+        targetElement.classList.add('submenu-loaded');
+        if (target && target.startsWith('#')) {
+            if(targetElement.classList.contains('submenu-active')) {
+                targetElement.ariaHidden = true;
+                resetSubmenus();
+            } else {
+                resetSubmenus();
+                targetElement.classList.add('submenu-active');
+                targetElement.ariaHidden = false;
+
+                links.forEach(link => {
+                    link.tabIndex = 0;
+                });
+
+            }
         };
     });
 });
