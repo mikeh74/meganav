fd12a1b4feb79e7078c598d6d5b9b41ae1670b89
horrocksm <horrocksm@ltegroup.co.uk>
2025-08-19 16:47:29 +0100

    Show/hide for submenu working

--- tmc/index.js
+++ tmc/index.js
@@ -2,18 +2,48 @@
 const mainNavImgs = document.querySelectorAll('[data-main-nav-img]');
 
 mainNavImgs.forEach(i => {
-
     let src = i.getAttribute('data-main-nav-img');
-    console.log(src);
-
     let img = new Image();
 
     img.src = src;
-
     img.onload = () => {
         i.style.backgroundImage = `url(${src})`;
         i.classList.add('active');
     };
 });
 
-console.log('Main nav images loaded');
+// wire up the main nav
+const mainNavSecondaryItems = document.querySelectorAll('.main-nav-secondary-item > a');
+
+mainNavSecondaryItems.forEach(item => {
+    item.addEventListener('click', (e) => {
+        e.preventDefault();
+        const target = item.getAttribute('href');
+
+        if (target.startsWith('#')) {
+            const targetElement = document.querySelector(target);
+            targetElement.classList.toggle('submenu-active');
+        };
+    });
+});
+
+/**
+ * function to find all elements with a class of target-expand and then loop
+ * throught them and add an event listener to each one that will take uses to
+ * the first link in the current element
+ */
+const targetExpandElements = document.querySelectorAll('.submenu-links-item');
+
+targetExpandElements.forEach(element => {
+    element.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        // find the first link in the current element
+        const firstLink = element.querySelector('a');
+        if (firstLink) {
+            window.location.href = firstLink.getAttribute('href');
+        } else {
+            console.warn('No link found in target expand element');
+        }
+    });
+});
